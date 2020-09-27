4610f1a27097594ae856a51a375271cfe525edab
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-27 22:15:44 +0530

    [Sai]Commit welcome message

---
+++ Line Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Lines problem!");
+        }
+    }
+}
