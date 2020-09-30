ebbd7b0f7ab4f325016b0d8541c2264d287e0499
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-30 21:27:04 +0530

    [Sai] Rverted. slight change in taking inputs

--- Line Problem/Program.cs
+++ Line Problem/Program.cs
@@ -11,17 +11,24 @@ namespace Line_Problem
             int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
 
 
-            Console.WriteLine("Enter the coordinates of X");
+            Console.WriteLine("Enter the Line coordinates");
 
+            Console.WriteLine("Enter the coordinates");
+
+            Console.WriteLine("x1: ");
             x1 = Convert.ToInt32(Console.ReadLine());
 
-            x2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the coordinates of Y");
-
+            Console.WriteLine("y1: ");
             y1 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Enter the coordinates of ");
+
+            Console.WriteLine("x2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("y2: ");
             y2 = Convert.ToInt32(Console.ReadLine());
+
 
             float length = (float) Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
             
